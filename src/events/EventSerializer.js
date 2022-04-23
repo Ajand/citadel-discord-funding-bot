a6f3e9ad0db5f9c1e640ff0396e61b8b64470444
Ajand Mardalizad <aseedgeek@gmail.com>
2022-04-23 23:54:16 +0430

    log parser added

--- src/events/EventSerializer.js
+++ src/events/EventSerializer.js
@@ -1,6 +1,11 @@
+const { ethers } = require("ethers");
 const EventModel = require("../models/Events");
 
+const FundingABI = require("../contracts/Funding.json").abi;
+
 const EventSerializer = (address, startingBlock = 0) => {
+  const iface = new ethers.utils.Interface(FundingABI);
+
   return new Promise((resolve, reject) => {
     EventModel.find(
       {
@@ -21,7 +26,7 @@ const EventSerializer = (address, startingBlock = 0) => {
       })
       .exec((err, tes) => {
         if (err) return reject(err);
-        return resolve(tes);
+        return resolve(tes.map((te) => ({ ...te._doc, ...iface.parseLog(te) })));
       });
   });
 };
