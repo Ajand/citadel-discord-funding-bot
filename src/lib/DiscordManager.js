a5602f8508faaac9059ba4595c10b0baab59257e
Ajand Mardalizad <aseedgeek@gmail.com>
2022-04-25 20:22:48 +0430

    Discord Manager Completed

--- src/lib/DiscordManager.js
+++ src/lib/DiscordManager.js
@@ -5,13 +5,25 @@ const LocalStorage = require("node-localstorage").LocalStorage;
 const config = require("../config.json");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-const localStorage = new LocalStorage(path.resolve(__dirname, "..", "data"));
+const localStorage = new LocalStorage(
+  path.resolve(__dirname, "..", "..", "data")
+);
 
 const targetChannels = new Map(
   localStorage.getItem("targetChannels")
     ? JSON.parse(localStorage.getItem("targetChannels"))
     : null
 );
+const removeFirstWord = (str) => {
+  console.log(str);
+  const indexOfSpace = str.indexOf(" ");
+
+  if (indexOfSpace === -1) {
+    return "";
+  }
+
+  return str.substring(indexOfSpace + 1);
+};
 
 const DiscordManager = async () => {
   await client.login(config.BOT_TOKEN);
@@ -19,12 +31,16 @@ const DiscordManager = async () => {
   client.on("messageCreate", function (message) {
     if (message.author.bot) return;
 
-    const commands = ["!start-price-tracker", "!stop-price-tracker"];
+    const commands = [
+      "!start-price-tracker",
+      "!stop-price-tracker",
+      "!set-notify-role",
+    ];
     if (
       message.content === commands[0] &&
       !targetChannels.get(message.channelId)
     ) {
-      targetChannels.set(message.channelId, message.author.id);
+      targetChannels.set(message.channelId, { author: message.author.id });
       localStorage.setItem(
         "targetChannels",
         JSON.stringify([...targetChannels])
@@ -34,7 +50,7 @@ const DiscordManager = async () => {
       message.content === commands[1] &&
       targetChannels.get(message.channelId)
     ) {
-      if (targetChannels.get(message.channelId) !== message.author.id) {
+      if (targetChannels.get(message.channelId).author !== message.author.id) {
         message.reply(`Only the channel setter can stop price tracker`);
       }
       targetChannels.delete(message.channelId);
@@ -43,6 +59,31 @@ const DiscordManager = async () => {
         JSON.stringify([...targetChannels])
       );
       message.reply(`Price tracker stopped`);
+    } else if (
+      message.content.startsWith(commands[2]) &&
+      targetChannels.get(message.channelId)
+    ) {
+      const roleName = removeFirstWord(message.content);
+
+      const foundRole = message.guild.roles.cache.find(
+        (role) => role.name === roleName
+      );
+
+      if (foundRole) {
+        targetChannels.set(message.channelId, {
+          ...targetChannels.get(message.channelId),
+          notifyRole: foundRole.id,
+        });
+        localStorage.setItem(
+          "targetChannels",
+          JSON.stringify([...targetChannels])
+        );
+        message.reply(`In case of emergency I will notify <@&${foundRole.id}>`);
+      } else {
+        message.reply(
+          `No role found. Are you sure your desired role is "${roleName}"?`
+        );
+      }
     } else {
       return;
     }
@@ -60,9 +101,24 @@ const DiscordManager = async () => {
     });
   };
 
-  const notifyUser = () => {};
+  const sendNotifyMessage = (message) => {
+    const channels = [...targetChannels.keys()];
 
-  return { sendMessage, notifyUser };
+    channels.forEach((channel) => {
+      const notifyRole = targetChannels.get(channel).notifyRole;
+
+      if (notifyRole && client.channels.cache.get(channel)) {
+        const transformedMessage = message.replace(
+          "::notifyRole::",
+          `<@&${notifyRole}>`
+        );
+
+        client.channels.cache.get(channel)?.send(transformedMessage);
+      }
+    });
+  };
+
+  return { sendMessage, sendNotifyMessage };
 };
 
 module.exports = DiscordManager;
